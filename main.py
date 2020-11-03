ef890901040f21c5b3ddedda34c93902115569d1
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-03 19:11:02 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import sympy as sy
+
+# I have defined the symbols for you to use in your expressions
+x, y, k, u, v = sy.symbols('x y k u v')
+# You need to add a sympy command after the comma here to calculate 
+# P(X < u and Y < v")
+cdf = 
+print("P(X < u and Y < v) =", cdf  )
